4f611c2aaad63fe537ebd3f3157ed2d4a03641d1
gZanda <gabrielfzanda@gmail.com>
2023-06-16 17:05:32 -0300

    Adjust

---
+++ Extra-ReactAndSpring/Back/src/test/java/Spring/Back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
